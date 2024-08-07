df58f3b3579b3260e57b07ece9fd469620fa8749
andrewtran28 <tran.andrew@outlook.com>
2024-08-07 10:32:17 -0400

    Function to clone divs in a row

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
+const CANVAS_SIZE = 16;
 let canvas = document.querySelector(".container");
-let square = document.createElement("div");
+let grid = new Array(CANVAS_SIZE);
 
-square.classList.add("square");
-canvas.appendChild(square);
+for (let i = 0; i < CANVAS_SIZE; i++) {
+    grid[i] = document.createElement("div");
+    grid[i].classList.add("square");
+    grid[i].textContent = "i" + i; //TEST: To see index of squares
+    canvas.appendChild(grid[i]);
+
+    grid[i].addEventListener("mouseover", () => {
+        grid[i].style.backgroundColor = 'red';
+    });
+    
+    grid[i].addEventListener("mouseout", () => {
+        grid[i].style.backgroundColor = 'white';
+    });
+}
