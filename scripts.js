e0d31521b649d750764736c8c625f36f0e314629
andrewtran28 <tran.andrew@outlook.com>
2024-08-08 08:10:51 -0400

    Revise grid and square creation

--- scripts.js
+++ scripts.js
@@ -1,28 +1,27 @@
+//Global Variables
+const canvas = document.querySelector(".container");
 const CANVAS_SIZE = 16;
-let canvas = document.querySelector(".container");
-let grid = Array(CANVAS_SIZE).fill().map(() => Array(CANVAS_SIZE).fill());
-let newRow = Array(CANVAS_SIZE);
 
-for(let y = 0; y < CANVAS_SIZE; y++) {
-    let r = 0;
-    for (let x = 0; x < CANVAS_SIZE; x++) {
-        if (r == 0 ) {
-            newRow[r] = document.createElement("div");
-            canvas.appendChild(newRow[r]);
-        }
-        grid[y][x] = document.createElement("div");
-        grid[y][x].classList.add("square");
-        grid[y][x].textContent ="X" + x + "Y" + y; //TEST: To see index of squares
-        canvas.appendChild(grid[y][x]);
-    
-        grid[y][x].addEventListener("mouseover", () => {
-            grid[y][x].style.backgroundColor = 'red';
+function createGrid (CANVAS_SIZE) {
+    const SQUARE_SIZE = 1080 / CANVAS_SIZE;
+    const TOTAL_SQUARES = CANVAS_SIZE * CANVAS_SIZE;
+
+    for (let i = 0; i < TOTAL_SQUARES; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${SQUARE_SIZE}px`;
+        square.style.height = `${SQUARE_SIZE}px`;
+        canvas.appendChild(square);
+        
+        square.addEventListener("mouseenter", () => {
+            //mouse event listener function to replace here
+            square.style.backgroundColor = 'green';
         });
         
-        grid[y][x].addEventListener("mouseout", () => {
-            grid[y][x].style.backgroundColor = 'white';
+        square.addEventListener("mouseleave", () => {
+            square.style.backgroundColor = 'white';
         });
-        r++;
     }
 }
 
+createGrid(CANVAS_SIZE);
