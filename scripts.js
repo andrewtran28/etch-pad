e368c84564970c919eba044be57b5802458d3fc7
andrewtran28 <tran.andrew@outlook.com>
2024-08-08 12:05:01 -0400

    Stylized Etch-pad buttons

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 //Global Variables
-const canvas = document.querySelector(".container");
+const canvas = document.querySelector(".canvas");
 const btn_resize = document.querySelector(".resize");
 const btn_clear = document.querySelector(".clear");
 const btn_normal = document.querySelector(".normal");
 const btn_erase = document.querySelector(".erase");
-const btn_randomize = document.querySelector(".randomize");
+const btn_randomize = document.querySelector(".random");
 
 let inputSize = 16;
 let brush = 0;
 
 function createGrid (SQUARES_PER_SIZE) {
-    const SQUARE_SIZE = 960 / SQUARES_PER_SIZE;
+    const SQUARE_SIZE = 720 / SQUARES_PER_SIZE;
     const TOTAL_SQUARES = SQUARES_PER_SIZE * SQUARES_PER_SIZE;
 
     canvas.innerHTML = "";
@@ -31,9 +31,9 @@ function createGrid (SQUARES_PER_SIZE) {
 function brushColour() {
 
     if (brush == 0) {
-        r = 0;
-        g = 0;
-        b = 0;
+        r = 40;
+        g = 40;
+        b = 40;
     }
 
     else if (brush == 1) {
@@ -56,16 +56,17 @@ createGrid(16);
 btn_resize.addEventListener("click", () => {
     inputSize = parseInt(prompt("Enter the number of squares per side of the Etch-Pad. Number must be between 1-100"));
 
-    if (inputSize > 0 && inputSize < 100) {
+    if (inputSize > 0 && inputSize <= 100) {
         createGrid(inputSize);
     }
 
     else {
-        while (!(inputSize > 0 && inputSize < 100)) {
+        while (!(inputSize > 0 && inputSize <= 100)) {
             inputSize = parseInt(prompt("Enter the number of squares per side of the Etch-Pad.\n\nNote: The input must be a number between 1-100."));
-            createGrid(inputSize);
         }
     }
+
+    createGrid(inputSize);
 });
 
 btn_clear.addEventListener("click", () => {
