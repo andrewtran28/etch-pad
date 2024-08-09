0336ac40793de431378dca3eed214808f8a3448e
andrewtran28 <tran.andrew@outlook.com>
2024-08-09 11:12:02 -0400

    Implement toggle grid function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-//Global Variables
 const canvas = document.querySelector(".canvas");
 const toggleBrush = document.querySelector(".toggleBrush");
 const btn_resize = document.querySelector(".resize");
@@ -6,10 +5,13 @@ const btn_clear = document.querySelector(".clear");
 const btn_normal = document.querySelector(".normal");
 const btn_erase = document.querySelector(".erase");
 const btn_random = document.querySelector(".random");
+const btn_grid = document.querySelector(".toggleGrid");
 
+//Global Variables
 let inputSize = 16;
 let brush = 0;
 let toggle = 1;
+let toggleGrid = true;
 
 function createGrid (SQUARES_PER_SIZE) {
     const SQUARE_SIZE = 720 / SQUARES_PER_SIZE;
@@ -72,21 +74,44 @@ canvas.addEventListener("click", () => {
     }
 });
 
+btn_grid.addEventListener("click", () => {
+    const grid = document.querySelectorAll(".square");
+    if (toggleGrid == true) {
+        btn_grid.textContent = "Grid: OFF";
+        grid.forEach(square => {
+            square.style.border = "0px";
+        });
+        toggleGrid = false;
+    }
+
+    else if (toggleGrid == false) {
+        btn_grid.textContent = "Grid: ON";
+        grid.forEach(square => {
+            square.style.border = "1px solid black";
+        });
+        toggleGrid = true;
+    }
+});
+
+btn_clear.addEventListener("click", () => {
+    createGrid(inputSize);
+    btn_grid.textContent = "Grid: ON";
+    toggleGrid = true;
+});
+
 btn_resize.addEventListener("click", () => {
     let newSize = parseInt(prompt("Enter the number of squares per side of the Etch-Pad.\n\nNote: Number must be between 1-100."));
 
     if (newSize > 0 && newSize <= 100) {
         inputSize = newSize;
         createGrid(inputSize);
+        btn_grid.textContent = "Grid: ON";
+        toggleGrid = true;
     }
 
     else {
         window.alert("Etch-Pad canvas size must be a number between 1-100.");
     }
-});
-
-btn_clear.addEventListener("click", () => {
-    createGrid(inputSize);
 });
 
 btn_normal.addEventListener("click", () => {
